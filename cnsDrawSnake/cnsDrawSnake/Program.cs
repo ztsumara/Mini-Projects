593e0ebe18dbc320db36123e210977b94b3c6607
Alihan <yaganoff2014@gmail.com>
2023-09-27 14:07:09 +0300

    SnakeVertical

---
+++ cnsDrawSnake/cnsDrawSnake/Program.cs
@@ -0,0 +1,70 @@
+﻿int flag1 = 0;
+while (flag1 != 1)
+{
+    int flag =0;
+    Console.Write("count of string: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("count of column: ");
+    int k = Convert.ToInt32(Console.ReadLine());
+
+
+    Console.Write("clockwise/counterclockwise?: (1/2)");
+    string? clock = Console.ReadLine();
+    if (clock.ToLower() == "1")
+    {
+        flag = k-1;
+    }
+    else if (clock.ToLower() == "2")
+    {
+        
+        flag = 0;
+
+    }
+    for (int i = 1; i < n+1; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            if (i % 2 != 0)
+            {
+
+                Console.Write("■");
+            }
+            else
+            {
+                if (j == flag)
+                {
+                    Console.Write("■");
+                    if (flag == 0)
+                    {
+                        flag = k - 1;
+                    }
+                    else
+                    {
+                        flag = 0;
+                    }
+                    break;
+
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+
+        }
+
+        Console.WriteLine();
+    }
+    Console.WriteLine("Repeat? Yes/No");
+    string? repeat = Console.ReadLine();
+    if (repeat.ToLower() == "no")
+    {
+        flag1 = 1;
+    }
+    else if (repeat.ToLower() != "yes")
+    {
+        Console.WriteLine("bad value");
+        flag1 = 1;
+
+    }
+}
