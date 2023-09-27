91f706a3a1d4e2f91bfeae6ff0e912ab5d76d05a
Alihan <yaganoff2014@gmail.com>
2023-09-27 13:35:26 +0300

    Rect and Romb

---
+++ cnsDrawRectangle/cnsDrawRectangle/Program.cs
@@ -0,0 +1,53 @@
+﻿int flag = 0;
+while (flag != 1)
+{
+    Console.Write("Enter width: ");
+
+    int width = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter height: ");
+    int height = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Do you want to fill? Yes/No ");
+    string? answer = Console.ReadLine();
+    bool fill = false;
+    if (answer.ToLower() == "no")
+    {
+        fill = false;
+    }
+    else
+    {
+        fill = true;
+    }
+
+
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (fill || i == 0 || i == height - 1 || j == 0 || j == width - 1)
+            {
+                Console.Write("*");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("Repeat? Yes/No");
+    string? repeat = Console.ReadLine();
+    if (repeat.ToLower() == "no")
+    {
+        flag = 1;
+    }
+    else if (repeat.ToLower() != "yes")
+    {
+        Console.WriteLine("bad value");
+        flag = 1;
+
+    }
+}
---
+++ cnsDrawRomb/cnsDrawRomb/Program.cs
@@ -0,0 +1,53 @@
+﻿int flag = 0;
+while (flag != 1)
+{
+    Console.Write("count of string: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int space = n - 1;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < space; j++)
+        {
+            Console.Write(" ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write("* ");
+        }
+
+        Console.WriteLine();
+        space--;
+    }
+
+    space = 0;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < space; j++)
+        {
+            Console.Write(" ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write("* ");
+        }
+
+        Console.WriteLine();
+        space++;
+    }
+    Console.WriteLine("Repeat? Yes/No");
+    string? repeat = Console.ReadLine();
+    if (repeat.ToLower() == "no")
+    {
+        flag = 1;
+    }
+    else if (repeat.ToLower() != "yes")
+    {
+        Console.WriteLine("bad value");
+        flag = 1;
+
+    }
+}
