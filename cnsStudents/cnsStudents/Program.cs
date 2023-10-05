695e9dbfa70cb3beacfafab8c575ae794e6647c1
Alihan <yaganoff2014@gmail.com>
2023-10-05 17:55:18 +0300

    Student

---
+++ cnsStudents/cnsStudents/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace cnsStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student x = new();
+            x.name = "Иван";
+            x.surname = "Петров";
+            x.age = 19;
+            Console.WriteLine(x.GetFullName());
+        }
+    }
+}
---
+++ cnsStudents/cnsStudents/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace cnsStudents
+{
+    internal class Student
+    {
+        public string name { get; internal set; }
+        public string surname { get; internal set; }
+        public int age { get; internal set; }
+
+        internal string GetFullName()
+        {
+            return $"{name} {surname} ({age})";
+        }
+    }
+}
