d7e57e1af8aec9f9d817521a7d50747ac628c25b
Alihan <yaganoff2014@gmail.com>
2023-10-05 09:11:13 +0300

    Draw

---
+++ cnsDrawX/cnsDrawX/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите размер буквы X:");
+int size = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        if (i == j || i == size - 1 - j)
+        {
+            Console.Write("\\\\");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
