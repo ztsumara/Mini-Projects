23e590cc3ada09f6f979bc825702077fc22816e2
Alihan <yaganoff2014@gmail.com>
2023-10-05 11:08:02 +0300

    cnsRect

---
+++ cnsOOPrectangle/cnsOOPrectangle/MyRectangle.cs
@@ -0,0 +1,25 @@
+﻿namespace cnsOOPrectangle
+{
+    internal class MyRectangle
+    {
+        private int length;
+        private int width;
+
+        public MyRectangle()
+        {
+            length = 10;
+            width = 10;
+        }
+
+        public MyRectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        internal int GetArea()
+        {
+            return 123;
+        }
+    }
+}
---
+++ cnsOOPrectangle/cnsOOPrectangle/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace cnsOOPrectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyRectangle x = new();
+            Console.WriteLine(x.GetArea());
+            MyRectangle x2 = new(2,3);
+            Console.WriteLine(x2.GetArea());
+        }
+    }
+}
